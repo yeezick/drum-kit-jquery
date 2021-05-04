7939e035bc5b54d961586476ae6a8aafa0d12c6b
Erick Manrique <80067603+yeezick@users.noreply.github.com>
2021-05-04 13:16:30 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+var buttons = document.querySelectorAll('.drum')
+
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    musicalKey(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+
+  musicalKey(event.key);
+  buttonAnimation(event.key);
+
+});
+
+function musicalKey(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3')
+      tom1.play();
+
+      break;
+
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3')
+      tom2.play();
+
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3')
+      tom3.play();
+
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3')
+      tom4.play();
+
+      break;
+
+    case "j":
+      var crash = new Audio('sounds/crash.mp3')
+      crash.play();
+
+      break;
+
+    case "k":
+      var kick = new Audio('sounds/kick-bass.mp3')
+      kick.play();
+
+      break;
+
+    case "l":
+      var snare = new Audio('sounds/snare.mp3')
+      snare.play();
+
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
+//   alert("clickable");
+//buttons[i].addEventListener("play",tom1)
+//tom1.play();
